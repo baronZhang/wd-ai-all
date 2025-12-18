93998531ba9d0b32f8b097f089d5ecb9b511bc44
zhangwendeng <7788999z@gmail.com>
2025-12-18 19:26:11 +0800

    251218

---
+++ projects/wd-langchain/src/gemini.py
@@ -0,0 +1,23 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=1.0,  # Gemini 3.0+ defaults to 1.0
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+
+
+messages = [
+    (
+        "system",
+        "You are a helpful assistant that translates English to French. Translate the user sentence.",
+    ),
+    ("human", "I love programming."),
+]
+ai_msg = model.invoke(messages)
+print(ai_msg)
+print(ai_msg.content)
+print(ai_msg.text)
